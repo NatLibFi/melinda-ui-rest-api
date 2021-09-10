ace7b0463a63d7a52d8869cae04f8c09ffb0192c
Markus Koskimies <markus.koskimies@helsinki.fi>
2021-09-10 14:59:04 +0300

    Transform moved to separate file

--- src/muuntaja/route.js
+++ src/muuntaja/route.js
@@ -14,12 +14,7 @@ import {createLogger} from '@natlibfi/melinda-backend-commons';
 //import defaults from 'defaults';
 //import createClient from '@natlibfi/sru-client';
 //import {MARCXML} from '@natlibfi/marc-record-serializers';
-import _ from 'lodash';
-import {printToE} from './config/config-presets';
-import {MarcRecord} from '@natlibfi/marc-record';
-import createRecordMerger from '@natlibfi/marc-record-merge';
-import * as MergeValidation from './marc-record-merge-validate-service';
-import * as PostMerge from './marc-record-merge-postmerge-service';
+import {transformRecord} from './transform';
 
 // https://github.com/NatLibFi/marc-record-serializers
 
@@ -130,155 +125,11 @@ export default function (jwtOptions) { // eslint-disable-line no-unused-vars
 
     //logger.debug(JSON.stringify(base.overwrites));
 
-    const merged = await transformRecord(source);
+    const merged = await transformRecord(logger, source);
 
     //logger.debug(JSON.stringify(merged));
 
     res.json(merged);
-  }
-
-  //-----------------------------------------------------------------------------
-
-  async function transformRecord(sourceRecord) { // eslint-disable-line max-statements
-
-    logger.debug(`transformRecord`);
-
-    /* These will be delivered from frontend: */
-
-    /*
-    // Transform type & profile
-    const getMergeProfile = getState().getIn(['config', 'mergeProfiles', getState().getIn(['config', 'selectedMergeProfile']), 'record']);
-    const defaultProfile = getState().getIn(['config', 'mergeProfiles']);
-    const mergeProfile = getMergeProfile === undefined ? defaultProfile.first() : getMergeProfile;
-    const subrecordMergeType = getState().getIn(['config', 'mergeProfiles', getState().getIn(['config', 'selectedMergeProfile']), 'subrecords', 'mergeType']);
-
-    const mergeConfiguration = mergeProfile.get('mergeConfiguration');
-    const validationRules = mergeProfile.get('validationRules');
-    const postMergeFixes = mergeProfile.get('postMergeFixes');
-    const newFields = mergeProfile.get('newFields');
-
-    //const preferredState = getState().getIn(['targetRecord', 'state']);
-    const baseRecord = preferredState === 'EMPTY' ? mergeProfile.get('targetRecord') : getState().getIn(['targetRecord', 'record']);
-    //const preferredHasSubrecords = preferredState === 'EMPTY' ? false : getState().getIn(['targetRecord', 'hasSubrecords']);
-
-    //const sourceRecord = getState().getIn(['sourceRecord', 'record']);
-    //const otherRecordHasSubrecords = getState().getIn(['sourceRecord', 'hasSubrecords']);
-    */
-
-    const transformType = printToE;
-    //const transformProfile = transformType.defaults.default.record;
-    const transformProfile = transformType.kvp.default.record;
-
-    const baseRecord = transformProfile.targetRecord;
-    const transformConfiguration = transformProfile.mergeConfiguration;
-    const {validationRules} = transformProfile;
-    const {postMergeFixes} = transformProfile;
-    const {newFields} = transformProfile;
-
-    // Melinda-commons / subrecord picker
-    const sourceRecordHasSubrecords = false;
-    const baseRecordHasSubrecords = false;
-
-    //logger.debug(`Transform profile: ${JSON.stringify(transformProfile, null, 2)}`);
-    //logger.debug(`Transform record: ${JSON.stringify(transformRecord, null, 2)}`);
-
-    //logger.debug(JSON.stringify(sourceRecord));
-    //logger.debug(`Transform profile: ${JSON.stringify(transformProfile)}`);
-    //logger.debug(`Base record: ${JSON.stringify(baseRecord)}`);
-
-    //logger.debug(`Config: ${JSON.stringify(transformConfiguration, null, 2)}`);
-    //logger.debug(`Validation: ${JSON.stringify(validationRules)}`);
-
-    /*
-    const validationRulesClone = _.clone(validationRules);
-    if (subrecordMergeType === subrecordMergeTypes.DISALLOW_SUBRECORDS) {
-      validationRulesClone.push(MergeValidation.otherRecordDoesNotHaveSubrecords);
-      validationRulesClone.push(MergeValidation.preferredRecordDoesNotHaveSubrecords);
-    }
-    */
-
-    const valid = await MergeValidation.validateMergeCandidates(validationRules, baseRecord, sourceRecord, baseRecordHasSubrecords, sourceRecordHasSubrecords);
-
-    if (!valid) {
-      return null;
-    }
-
-    function appendNewFields(originalMergedRecord) {
-      if (!newFields) {
-        return originalMergedRecord;
-      }
-
-      const mergedRecord = new MarcRecord(originalMergedRecord);
-
-      newFields.forEach(field => {
-        const fields = mergedRecord.fields.filter(fieldInMerged => field.tag === fieldInMerged.tag && _.isEqual(field.subfields, fieldInMerged.subfields));
-
-        if (fields.length === 0) { // eslint-disable-line functional/no-conditional-statement
-          mergedRecord.appendField({...field});
-        }
-      });
-
-      return mergedRecord;
-    }
-
-    const merge = createRecordMerger(transformConfiguration);
-    const mergedRecord = appendNewFields(merge(baseRecord, sourceRecord));
-    const postMerged = PostMerge.applyPostMergeModifications(postMergeFixes, baseRecord, sourceRecord, mergedRecord);
-
-    logger.debug(`Notes: ${JSON.stringify(postMerged.notes)}`);
-    return postMerged.record;
-
-    /*
-    if (baseRecord && sourceRecord) { //targetRecord and sourceRecord
-      const merge = createRecordMerger(mergeConfiguration);
-      const validationRulesClone = _.clone(validationRules);
-      if (subrecordMergeType === subrecordMergeTypes.DISALLOW_SUBRECORDS) {
-        validationRulesClone.push(MergeValidation.otherRecordDoesNotHaveSubrecords);
-        validationRulesClone.push(MergeValidation.preferredRecordDoesNotHaveSubrecords);
-      }
-
-      MergeValidation.validateMergeCandidates(validationRulesClone, baseRecord, sourceRecord, preferredHasSubrecords, otherRecordHasSubrecords)
-        .then(() => merge(baseRecord, sourceRecord))
-        .then((originalMergedRecord) => {
-          if (!newFields) {
-            return originalMergedRecord;
-          }
-
-          const mergedRecord = new MarcRecord(originalMergedRecord);
-
-          newFields.forEach(field => {
-            const fields = mergedRecord.fields.filter(fieldInMerged => field.tag === fieldInMerged.tag && _.isEqual(field.subfields, fieldInMerged.subfields));
-
-            if (fields.length === 0) {
-              mergedRecord.appendField({...field, uuid: uuid.v4()});
-            }
-          });
-
-          return mergedRecord;
-        })
-        .then(mergedRecord => PostMerge.applyPostMergeModifications(postMergeFixes, baseRecord, sourceRecord, mergedRecord))
-        .then(result => {
-          dispatch(setMergedRecord(result.record));
-        })
-        .catch(exceptCoreErrors(error => {
-          dispatch(setMergedRecordError(error));
-        }));
-
-      // find pairs for subrecods
-      const sourceSubrecordList = sourceSubrecords(getState());
-      const targetSubrecordList = targetSubrecords(getState());
-
-      const matchedSubrecordPairs = match(sourceSubrecordList, targetSubrecordList);
-
-      dispatch(updateSubrecordArrangement(matchedSubrecordPairs));
-
-      if (subrecordMergeType === subrecordMergeTypes.MERGE || subrecordMergeType === subrecordMergeTypes.SHARED) {
-        dispatch(updateMergedSubrecords(matchedSubrecordPairs));
-      }
-    }
-
-    /**/
-    //return mergedRecord;
   }
 }
 
