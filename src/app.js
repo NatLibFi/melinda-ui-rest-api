ad55aab51c13254f462fb94a611718774279857d
Markus Koskimies <markus.koskimies@helsinki.fi>
2022-06-10 09:00:01 +0300

    Organizing & route for viewer
    CSS's and images moved to clients/common/ for access accross clients. New route opened for viewer.

--- src/app.js
+++ src/app.js
@@ -61,7 +61,8 @@ export default function ({
     app.use('/rest/muuntaja', passport.authenticate(['melinda', 'jwt'], {session: false}), createMuuntajaRouter(sruUrl));
     app.use('/test', express.static(path.join(__dirname, 'clients/test'), {index: 'testclient.html'}));
     app.use('/muuntaja', express.static(path.join(__dirname, 'clients/muuntaja/'), {index: 'muuntaja.html'}));
-    app.use('/common', express.static(path.join(__dirname, 'clients/common/'), {index: 'index.html'}));
+    app.use('/viewer', express.static(path.join(__dirname, 'clients/viewer/'), {index: 'viewer.html'}));
+    app.use('/common', express.static(path.join(__dirname, 'clients/common/')));
     app.use('/', express.static(path.join(__dirname, 'clients/login/'), {index: 'login.html'}));
     app.use(handleError);
 
