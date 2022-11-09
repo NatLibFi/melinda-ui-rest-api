03dad0723ed223a0315632a8e11d63eb391367e5
natlibesa <31346486+natlibesa@users.noreply.github.com>
2022-11-09 09:55:25 +0200

    MUU-190: Checked field 490 indicators.

--- src/artikkelit/generate/generate4xxFields.js
+++ src/artikkelit/generate/generate4xxFields.js
@@ -2,7 +2,7 @@
 // https://www.kiwi.fi/pages/viewpage.action?pageId=155648846 -> toistuva palsta
 export function generatef490(sectionOrColumn = false) {
   if (sectionOrColumn) {
-    return [{tag: '490', ind1: ' ', ind2: ' ', subfields: [{code: 'a', value: sectionOrColumn}]}];
+    return [{tag: '490', ind1: '0', ind2: ' ', subfields: [{code: 'a', value: sectionOrColumn}]}];
   }
 
   return [];
