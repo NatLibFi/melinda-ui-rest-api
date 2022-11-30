e9277208386f9199e08a16b047a00c74d95648fd
natlibesa <31346486+natlibesa@users.noreply.github.com>
2022-11-30 08:36:17 +0200

    MUU-215: Fields 100 & 700, subfield c - generate titles

--- src/artikkelit/generate/generate1xxFields.js
+++ src/artikkelit/generate/generate1xxFields.js
@@ -13,12 +13,12 @@ export function generatef100sf110sf700sf710s(authors = []) {
 
     function generate100or110() {
       if (authors[0].firstName === ' ' && authors[0].lastName === ' ') {
-        return []; // hit space key on both means skip this
+        return []; // hit space key on both means: skip this
       }
 
       return {
         tag: author.relator === 'yhteisö' ? '110' : '100',
-        ind1: author.relator === 'yhteisö' ? '2' : '1',
+        ind1: ind1Value(),
         ind2: ' ',
         subfields: author.relator === 'yhteisö' ? generateSubfieldsForYhteiso() : generateSubfieldsForPerson()
       };
@@ -28,21 +28,56 @@ export function generatef100sf110sf700sf710s(authors = []) {
     function generate700or710() {
       return {
         tag: author.relator === 'yhteisö' ? '710' : '700',
-        ind1: author.relator === 'yhteisö' ? '2' : '1',
+        ind1: ind1Value(),
         ind2: ' ',
         subfields: author.relator === 'yhteisö' ? generateSubfieldsForYhteiso() : generateSubfieldsForPerson()
       };
     }
 
+    function ind1Value() {
+      const firstNameSplitted = author.firstName.split(' ');
+      const [firstChar] = firstNameSplitted;
+
+      if (firstChar === 'c' && firstNameSplitted.length > 1) {
+        return '0';
+      }
+
+      const ind1chkd = author.relator === 'yhteisö' ? '2' : '1';
+      return ind1chkd;
+    }
+
     function generateSubfieldsForPerson() { // 100 & 700
       const subA = {code: 'a', value: `${author.lastName}, ${author.firstName},`};
+      const subAC = {code: 'a', value: `${author.lastName},`}; // used only with C-subfield (title case)
       const subE = {code: 'e', value: `${author.relator}.`};
       const mapOrgnNames = author.authorsTempOrganizations.map(elem => `${elem.organizationName}`);
+
+      function doSubC() { // title case, when in field firstname is: c+title
+        const firstNameSplitted = author.firstName.split(' ');
+        const [firstChar] = firstNameSplitted;
+
+        if (firstChar === 'c' && firstNameSplitted.length > 1) {
+          const theTitle = `${author.firstName.slice(2)},`;
+          const outputC = {code: 'c', value: theTitle};
+          return outputC;
+        }
+
+        return false;
+      }
 
       if (mapOrgnNames.length > 0) {
         const subU = generateOrganizations();
         const subG = generateOrgnCodes();
+
+        if (doSubC()) {
+          return [subAC, doSubC(), subE, subU, subG];
+        }
+
         return [subA, subE, subU, subG];
+      }
+
+      if (doSubC()) {
+        return [subAC, doSubC(), subE];
       }
 
       return [subA, subE];
