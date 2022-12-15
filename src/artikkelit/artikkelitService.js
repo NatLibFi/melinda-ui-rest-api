ea0b0558fc537eacefa3d842d76f5bf2739da224
natlibesa <31346486+natlibesa@users.noreply.github.com>
2022-12-15 09:48:59 +0200

    MUU-191: field 591, subfield d  & added new selection on list(D2 - Artikkeli...)

--- src/artikkelit/artikkelitService.js
+++ src/artikkelit/artikkelitService.js
@@ -25,11 +25,7 @@ export function createArtikkelitService() {
     const {isElectronic, publishing} = source;
     const {issn, melindaId} = parseIncomingData(source);
     const {language: articleLanguage, title: articleTitle, titleOther: articleTitleOther} = article;
-
-    const articleCategory = article.type;
-    console.log('   ***   artikkelitService.js - articleCategory = article.type =', articleCategory); // eslint-disable-line   
-
-
+    const articleCategory = article.type; // for field 591
     const sourceType = getSourceType(source);
     const SourceTypeAsCode = source.sourceType; // eg. 'nnas', 'nnam' for field 773
     const abstractLanguages = abstracts.map(elem => elem.language.iso6392b);
@@ -57,7 +53,7 @@ export function createArtikkelitService() {
         ...generatef500(notes), // general notes
         ...generatef520(abstracts), // Abstracts
         ...generatef567(metodologys),
-        ...generatef591(sourceType, sciences, articleCategory), // added: articleCategory
+        ...generatef591(sourceType, sciences, articleCategory),
         ...generatef593(journalJufo, year),
         ...generatef598(), // local notes (lisäkentät)
         ...generatef599(f599a, f599x),
