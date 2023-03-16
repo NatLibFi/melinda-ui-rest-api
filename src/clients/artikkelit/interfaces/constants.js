2d2c24bc310d15ed3f1d57129740b4c19f9efdb5
natlibfi-tonifred <117169187+natlibfi-tonifred@users.noreply.github.com>
2023-03-16 07:59:25 +0200

    Record preview is now possible without source record (#90)

--- src/clients/artikkelit/interfaces/constants.js
+++ src/clients/artikkelit/interfaces/constants.js
@@ -992,3 +992,128 @@ export const sectionOrColumnList = [
     text: ""
   }
 ]
+
+export const journalTemplate = {
+  authors: [
+    {
+      asteriId: "000001234",
+      lastName: "Malliyhteisö",
+      organisations: [],
+      relator: "yhteisö"
+    }
+  ],
+  isElectronic: false,
+  isbns: [],
+  issns: [
+    "0123-4567"
+  ],
+  notices: [],
+  publisherInfo: {
+    publisher: "Mallijulkaisija",
+    publisherLocation: "Helsinki",
+    publisherYears: {
+      start: "2022",
+      end: "2023"
+    }
+  },
+  publishing: "Helsinki : Mallijulkaisija, 2022-2023.",
+  recordType: "Kausijulkaisu",
+  simpleElements: {
+    articleLanguage: [
+      "mul"
+    ],
+    articleReference: [],
+    articleSciences: [],
+    f600s: [],
+    f610s: [],
+    f648s: [],
+    f650s: [],
+    f651s: [],
+    f653s: [],
+    f655s: [],
+    localNotes598: [],
+    methodology: [],
+    noteGeneral: [],
+    referenceLink: [],
+    titleOther: [
+      "Testijulkaisu",
+      "Test publikation"
+    ]
+  },
+  simpleGroups: {
+    abstract: {
+      language: [],
+      term: []
+    },
+    localNotes599: [],
+    otherClassificationNumber: [],
+    universalDecimalClassificationNumber: []
+  },
+  sourceIds: [
+    "(FI-MELINDA)001234567"
+  ],
+  sourceType: "nnas",
+  title: "Mallijulkaisu",
+  years: "2022-2023."
+}
+
+export const bookTemplate = {
+  authors: [
+    {
+      asteriId: "000004321",
+      firstName: "Malli",
+      lastName: "Mallailija",
+      organisations: [],
+      relator: "kirjoittaja"
+    }
+  ],
+  isElectronic: false,
+  isbns: [
+    "123-45-6789-0"
+  ],
+  issns: [],
+  notices: [],
+  publisherInfo: {
+    publisher: "Helsingin Mallijulkaisijat ry",
+    publisherLocation: "Helsinki",
+    publisherYears: {
+      start: "2023"
+    }
+  },
+  publishing: "Helsinki : Helsingin Mallijulkaisijat ry, 2023",
+  recordType: "Monografia",
+  simpleElements: {
+    articleLanguage: [
+      "fin"
+    ],
+    articleReference: [],
+    articleSciences: [],
+    f600s: [],
+    f610s: [],
+    f648s: [],
+    f650s: [],
+    f651s: [],
+    f653s: [],
+    f655s: [],
+    localNotes598: [],
+    methodology: [],
+    noteGeneral: [],
+    referenceLink: [],
+    titleOther: []
+  },
+  simpleGroups: {
+    abstract: {
+      language: [],
+      term: []
+    },
+    localNotes599: [],
+    otherClassificationNumber: [],
+    universalDecimalClassificationNumber: []
+  },
+  sourceIds: [
+    "(FI-MELINDA)007654321"
+  ],
+  sourceType: "nnam",
+  title: "Kirjamalli",
+  years: "2023"
+}
