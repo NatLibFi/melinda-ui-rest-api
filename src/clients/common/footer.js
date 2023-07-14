5ca4f51021ced2d2d4181be6d4de433e98c4b99a
natlibfi-ptalosel <pinja.talosela@helsinki.fi>
2023-07-14 12:07:18 +0300

    Add draft for footer js

---
+++ src/clients/common/footer.js
@@ -0,0 +1,10 @@
+class Footer extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // write element functionality in here
+    }
+}
+
+customElements.define("footer", Footer);
