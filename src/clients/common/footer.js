f858075e273025fa9cf9a882bc8debcc11363ec4
natlibfi-ptalosel <pinja.talosela@helsinki.fi>
2023-07-14 13:24:34 +0300

    Add function to create and append melinda footer to app

--- src/clients/common/footer.js
+++ src/clients/common/footer.js
@@ -1,10 +1,44 @@
+//*****************************************************************************
+//
+// Melinda footer custom element for apps
+//
+//*****************************************************************************
+
 class Footer extends HTMLElement {
     constructor() {
-        // Always call super first in constructor
         super();
+    }
 
-        // write element functionality in here
+    connectedCallback() {
+
+        getFooterHtml()
+            .then(html => {
+                const footer = getFooterElement(html);
+                this.appendChild(footer);
+                console.log('Footer is added: ', footer.isConnected);
+            })
+            .catch(error =>
+                console.log('Error while setting footer: ', error)
+            )
+
+
+
+        async function getFooterHtml() {
+            const response = await fetch('/../common/templates/footer.html');
+            const html = await response.text();
+            return html;
+        }
+
+        function getFooterElement(html) {
+            const parser = new DOMParser();
+            const footerDocument = parser.parseFromString(html, 'text/html');
+            const footerTemplate = footerDocument.getElementById('footerTemplate');
+            const footerFragment = footerTemplate.content.cloneNode(true);
+            const footerElement = footerFragment.getElementById('footer');
+            return footerElement;
+        }
+
     }
 }
 
-customElements.define("footer", Footer);
+customElements.define('melinda-footer', Footer);
