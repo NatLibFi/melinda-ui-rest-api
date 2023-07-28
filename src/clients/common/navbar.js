3dacde441314379b306e89e7ad3555a92e7fb3e6
natlibfi-ptalosel <pinja.talosela@helsinki.fi>
2023-07-28 16:26:27 +0300

    Add new melinda-navbar custom element

--- src/clients/common/navbar.js
+++ src/clients/common/navbar.js
@@ -3,3 +3,40 @@
 // Melinda navigation bar custom element for apps                             //
 //                                                                            //
 //****************************************************************************//
+
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+
+    getNavbarHtml()
+      .then(html => {
+        const navbar = getNavbarElement(html);
+        this.appendChild(navbar);
+        console.log('Navbar is added: ', navbar.isConnected);
+      })
+      .catch(error =>
+        console.log('Error while setting navbar: ', error)
+      )
+
+    async function getNavbarHtml() {
+      const response = await fetch('/../common/templates/navbar2.html');
+      const html = await response.text();
+      return html;
+    }
+
+    function getNavbarElement(html) {
+      const parser = new DOMParser();
+      const navbarDocument = parser.parseFromString(html, 'text/html');
+      const navbarTemplate = navbarDocument.getElementById('navbarTemplate');
+      const navbarFragment = navbarTemplate.content.cloneNode(true);
+      const navbarElement = navbarFragment.getElementById('navbar');
+      return navbarElement;
+    }
+
+  }
+}
+
+customElements.define('melinda-navbar', Navbar);
