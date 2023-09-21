25d75885a416833d59f95867e340515494b0ad48
natlibesa <31346486+natlibesa@users.noreply.github.com>
2023-09-21 10:54:34 +0300

    MUU-65, statusRoute, ping. Commit 4, checked.

--- src/utilRoute/statusRoute.js
+++ src/utilRoute/statusRoute.js
@@ -2,11 +2,11 @@ import httpStatus from 'http-status';
 import {Router} from 'express';
 import {createLogger} from '@natlibfi/melinda-backend-commons';
 
-export default function createPingRoute() {// eslint-disable-line no-unused-vars
+export default function createPingRoute() { // eslint-disable-line no-unused-vars
   const logger = createLogger();
 
   return new Router()
-  .use('/', ping)
+    .use('/', ping);
 
   function ping() {
     logger.log('info', `statusRoute/ping, httpStatus: ${httpStatus.OK}`);
