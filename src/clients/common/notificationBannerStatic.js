508c3cae70b3d93337ad09424570b6300a0b30df
natlibfi-kmskuusi <118263770+natlibfi-kmskuusi@users.noreply.github.com>
2023-12-19 14:15:24 +0200

    MUU-348: static banner added, test case on muuntaja

---
+++ src/clients/common/notificationBannerStatic.js
@@ -0,0 +1,102 @@
+
+/**
+ * In a same:ish way the notification banner and dialogs are handled push child to a container except here make sure theres only one child (for now)
+ */
+/**
+ * 
+ * @param {*} content we are expecting object with a "type" and "message"
+ * @returns 
+ */
+export function showNotificationBannerStatic(content) {
+
+  if (arguments.length === 0 || content === null) {
+    console.log('NotificationBannerStatic needs arguments');
+    return;
+  }
+  if(!content || !content.type || !content.message){
+    console.log('showNotificationBannerStatic argument needs to have object with correct fields, see JSDoc comment');
+    return;
+  }
+
+  getNotificationBannerStaticHtml()
+    .then(html => createNotificationBannerStatic(content, html))
+    .catch(error => console.log('Error while fetching NotificationBannerStatic html: ', error));
+}
+
+async function getNotificationBannerStaticHtml() {
+  const response = await fetch('/../common/templates/notificationBannerStatic.html');
+  const html = await response.text();
+  return html;
+}
+
+function createNotificationBannerStatic(content, html) {
+  const templateElement = getNewNotificationBannerStatic(html);
+  
+  styleBasedOnType();
+  addTextToElement();
+  addElementToContainer();
+
+  //************************************************************************************** */
+  // Helper functions for createNotificationBannerStatic:
+
+  function getNewNotificationBannerStatic(html) {
+    const NotificationBannerStaticDocument = getNotificationBannerStaticDocument();
+    const NotificationBannerStaticTemplate = NotificationBannerStaticDocument.getElementById('notificationBannerStaticTemplate');
+    const NotificationBannerStaticFragment = NotificationBannerStaticTemplate.content.cloneNode(true);
+    const NotificationBannerStaticElement = NotificationBannerStaticFragment.getElementById('notificationBannerStatic');
+    return NotificationBannerStaticElement;
+
+    function getNotificationBannerStaticDocument() {
+      const parser = new DOMParser();
+      const NotificationBannerStaticDocument = parser.parseFromString(html, 'text/html');
+      return NotificationBannerStaticDocument;
+    }
+
+  }
+  function styleBasedOnType(){
+    
+    let backgroundColor = '';
+    let iconColor = '';
+    let icon = '';
+
+    switch (content.type) {
+      case 'success':
+        backgroundColor = 'var(--color-green-80)';
+        iconColor = 'var(--color-green-100)';
+        icon = 'check_circle_outline';
+        break;
+      case 'alert':
+        backgroundColor = 'var(--color-yellow-80)';
+        iconColor = 'var(--color-blue-100)';
+        icon = 'warning_amber';
+      break;
+      case 'error':
+        backgroundColor = 'var(--color-red-80)';
+        iconColor = 'var(--color-red-100)';
+        icon = 'report_gmailerrorred';
+      break;
+    
+      default:
+        // style 'info' is the default status message
+        // notificationDialog has blue background with info icon
+        backgroundColor = 'var(--color-blue-60)';
+        iconColor = 'var(--color-blue-100)';
+        icon = 'error_outline';
+        break;
+    }
+    const container = document.getElementById('notificationStaticBanner');
+    container.style.setProperty(`--style-background-color`, backgroundColor);
+    templateElement.style.setProperty(`--style-icon-color`, iconColor);
+    templateElement.querySelector(`.notificationbannerstatic-icon .material-icons`).innerHTML = icon;
+  }
+  function addTextToElement(){
+    templateElement.querySelector(`.notificationbannerstatic-text`).innerHTML = content.message;
+  }
+
+  //keep single child element
+  function addElementToContainer() {
+    const container = document.getElementById('notificationStaticBanner');
+    container.replaceChildren();//only keep one item, TODO: support more ?
+    container.prepend(templateElement);
+  }
+}
