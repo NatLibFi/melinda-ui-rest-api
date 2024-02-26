dcc7776b5f8af17eae9d2233c79060ae0fb98622
natlibesa <31346486+natlibesa@users.noreply.github.com>
2024-02-26 11:16:07 +0200

    Muu 412 (#246)
    * MUU-380, -377 & -401: 5xx-files modified
    
    * MUU-408: Excluded field 538 on case KVP/e2p. Also checked other 5xx fields.
    
    * MUU-408: Excluded field 538 on case KVP/e2p. Also checked other 5xx fields (vII).
    
    * MUU-409: field 579 excluded when KVP/e2p; MUU-410: exclude all 599 fields.
    
    * MUU-409: field 579 excluded when KVP/e2p; MUU-410: exclude all 599 fields. Check.
    
    * MUU-407: added field 300 for case KVP/e2p. Correction on MUU-408: double zeros issue.
    
    * MUU-407: added field 300 for case KVP/e2p. Correction on MUU-408: double zeros issue. Checked.
    
    * MUU-412:exclude field 653 when case KVP/e2p.
    
    * MUU-502: field 041 now only once (was double)
    
    * Fixes
    
    ---------
    
    Co-authored-by: Markus Koskimies <markus.koskimies@helsinki.fi>

--- src/muuntaja/config/e-to-print/createBaseRecord.js
+++ src/muuntaja/config/e-to-print/createBaseRecord.js
@@ -83,7 +83,7 @@ function getReducers(options) {
     fillDefault('008'),
     fillDefault('020'),
     // fillDefault('040'),
-    fillDefault('041'),
+    //fillDefault('041'), // MUU-502
     // fillDefault('042'),
     // fillDefault('300'),
     fillDefault('337'),
